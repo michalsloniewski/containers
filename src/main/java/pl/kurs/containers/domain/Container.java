f6f9ee7f96f4a9d01ff0c16cb0ca8b00e0ee5fe3
Michal Sloniewski <michal.sloniewski.java@gmail.com>
2023-02-27 13:02:52 +0100

    Added comment co Container class

--- src/main/java/pl/kurs/containers/domain/Container.java
+++ src/main/java/pl/kurs/containers/domain/Container.java
@@ -1,2 +1,60 @@
-package pl.kurs.containers.domain;public class Container {
+package pl.kurs.containers.domain;
+
+import java.io.Serializable;
+
+/**
+ * Container is a typical "java bean" class
+ *
+ * */
+
+public class Container implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private String name;
+    private double maxCapacity;
+    private double waterLevel;
+
+    public Container() {
+    }
+
+    public Container(String name, double maxCapacity, double waterLevel) {
+        this.name = name;
+        this.maxCapacity = maxCapacity;
+        this.waterLevel = waterLevel;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getMaxCapacity() {
+        return maxCapacity;
+    }
+
+    public void setMaxCapacity(double maxCapacity) {
+        this.maxCapacity = maxCapacity;
+    }
+
+    public double getWaterLevel() {
+        return waterLevel;
+    }
+
+    public void setWaterLevel(double waterLevel) {
+        this.waterLevel = waterLevel;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "Container{" +
+                "name='" + name + '\'' +
+                ", maxCapacity=" + maxCapacity +
+                ", waterLevel=" + waterLevel +
+                '}';
+    }
 }
